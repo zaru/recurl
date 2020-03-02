753cca2fe4d2d560b17321e1058838b8d7aa128d
zaru <zarutofu@gmail.com>
2020-03-02 23:46:59 +0900

    request to curl command

--- demo/sinatra-app/config.ru
+++ demo/sinatra-app/config.ru
@@ -1,4 +1,8 @@
+require 'rack'
+require 'logger'
+
 require File.expand_path 'recurl', File.dirname(__FILE__)
+Recurl.logger = Logger.new('./recurl.log')
 use Recurl
 
 require File.expand_path 'app', File.dirname(__FILE__)
--- demo/sinatra-app/recurl.rb
+++ demo/sinatra-app/recurl.rb
@@ -1,9 +1,56 @@
+require 'pry'
+require 'logger'
+
 class Recurl
+
+  @@logger = Logger.new(STDOUT)
+
+  def self.logger=(logger)
+    @@logger = logger
+  end
+
   def initialize(app)
     @app = app
   end
 
   def call(env)
+    @env = env
+    build
     @app.call env
+  end
+
+  private
+
+  def build
+    logger.info "curl #{http_method} #{headers} #{params} #{url}"
+  end
+
+  def headers
+    {
+      'User-Agent' => env['HTTP_USER_AGENT'],
+      'Accept' => env['HTTP_ACCEPT'],
+      'Cookie' => env['HTTP_COOKIE']
+    }.map{|k,v| "'#{k}: #{v}'"}.join(' -H ')
+  end
+
+  def params
+    body = Rack::Request.new(env).body.read
+    body.empty? ? '' : "-d '#{body}'"
+  end
+
+  def url
+    "'#{env['REQUEST_URI']}'"
+  end
+
+  def http_method
+    "-X #{env['REQUEST_METHOD']}"
+  end
+
+  def env
+    @env
+  end
+
+  def logger
+    @@logger
   end
 end
